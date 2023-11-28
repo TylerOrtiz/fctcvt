c37b6bf885ae52753cbd5a2e3456fbbe1dae5bd1
Tyler O <ortizt89@gmail.com>
2023-11-28 00:48:11 -0500

    FCTC-posts and shows pull

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,65 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { client } from '@/api/contentful'
+import { parseISO, format } from 'date-fns';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-export default function Home() {
+
+async function getShows() {
+  const data = await client.getEntries({ content_type: 'show' })
+  return data.items
+}
+
+async function getPosts() {
+  const data = await client.getEntries({ content_type: 'blogPost' })
+  return data.items
+}
+
+
+export default async function Home() {
+  const shows = await getShows()
+  const posts = await getPosts()
+  const options = {
+    preserveWhitespace: true,
+  };
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+
+      <div className={styles.grid}>
+        <h1>Shows</h1>
+        {shows.map(show => {
+          return (
+            <div key={show.fields.title}>
+              {show.fields.title}
+              <ul>
+                {show.fields.showDatesAndTimes.map(showtime => {
+                  const date = parseISO(showtime.fields.showTime)
+                  return (<li key={showtime.fields.showId}>
+                    {format(date, 'LLLL d, yyyy')}
+                  </li>)
+                })}
+              </ul>
+
+            </div>
+          )
+        })}
       </div>
 
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <h1>Blog Posts</h1>
+        {posts.map(post => {
+          const postDate = parseISO(post.fields.postDate)
+          return (
+            <>
+              <div key={post.fields.postTitle}>
+                <h2>{post.fields.postTitle}</h2>
+                <h3>{format(postDate, 'LLLL d, yyyy')}</h3>
+              </div>
+              <div>{documentToReactComponents(post.fields.postContent, options)}</div>
+            </>
+          )
+        })}
       </div>
     </main>
   )
