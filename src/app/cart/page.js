e50bc399f7488cb7671328e6915d446de0612c04
Tyler O <ortizt89@gmail.com>
2024-05-04 14:12:05 -0400

    Cart rendering items, cart nav linked to cart page.

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,12 +1,24 @@
+'use client'
 import { Button } from '@mui/material';
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Unstable_Grid2'
+import { updateCart, getCart } from '@/cart/cart';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
+export default function Page({ params }) {
+    const queryClient = useQueryClient()
+    const { data: cart } = useQuery({
+        queryKey: ['cart'],
+        queryFn: () =>
+            getCart(),
+    })
 
-export default async function Page({ params }) {
     const Cart = () => (
         <>
             <h2>Items:</h2>
+            {cart?.items?.map((item, index) => (
+                <div key={index}>{item}</div>
+            ))}
         </>
     )
 
--- src/component/CartTracker/CartTrackerNav.js
+++ src/component/CartTracker/CartTrackerNav.js
@@ -6,15 +6,22 @@ import IconButton from '@mui/material/IconButton';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useQuery } from '@tanstack/react-query';
 import { getCart } from '@/cart/cart';
+import { useRouter } from 'next/navigation';
 
 export default function CartTrackerNav() {
+    const router = useRouter();
     const { data: cart } = useQuery({
         queryKey: ['cart'],
         queryFn: () =>
             getCart(),
     })
+
+    const goToCart = () => {
+        router.push(`/cart`);
+    }
+
     return (<>
-        <IconButton aria-label="cart">
+        <IconButton aria-label="cart" onClick={goToCart}>
             <Badge badgeContent={cart?.items?.length ?? 0} color="secondary">
                 <ShoppingCartIcon />
             </Badge>
