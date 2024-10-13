c2a5cd4d78a6c83e532ae7c8af5f9e487a9ba0c9
Tito <ortizt89@gmail.com>
2024-10-13 09:26:31 -0400

    Error messaging for checkout flow.

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,30 +1,38 @@
 'use client'
+import { useState } from 'react';
 import { Button } from '@mui/material';
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Unstable_Grid2'
 import { updateCart, getCart } from '@/cart/cart';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
+import Alert from '@mui/material/Alert';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 export default function Page({ params }) {
-    const queryClient = useQueryClient()
     const { data: cart } = useQuery({
         queryKey: ['cart'],
         queryFn: () =>
             getCart(),
     })
+    const [errorMessage, setErrorMessage] = useState(null)
 
     const checkout = async () => {
-        const response = await fetch('/api/checkout', {
-            method: 'POST',
-            body: JSON.stringify({ lineItems: cart?.items }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-
-        console.log("checkout response", response)
-        const paylaod = await response.json()
-        window.location.href = paylaod.url
+        try {
+            setErrorMessage(null)
+            const response = await fetch('/api/checkout', {
+                method: 'POST',
+                body: JSON.stringify({ lineItems: cart?.items }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            const paylaod = await response.json()
+            window.location.href = paylaod.url
+        } catch (error) {
+            setErrorMessage('There was an error processing your request. Please try again later.')
+            console.error(error?.message)
+        }
+      
     }
 
     const Cart = () => (
@@ -46,7 +54,12 @@ export default function Page({ params }) {
     return <>
         <Box sx={{ marginLeft: '1rem', marginRight: '1rem' }}>
             <h1>Cart</h1>
-
+            {errorMessage ? <>
+                <Alert severity="error"
+                    icon={<ErrorOutlineIcon fontSize="inherit"></ErrorOutlineIcon>}
+                ><span>{errorMessage}</span></Alert>
+            </> : null
+            }
             <Grid container columns={{ xs: 4, sm: 8, md: 12 }} spacing={4}>
                 <Grid xs={4} sm={8} md={12} >
                     <Cart />
